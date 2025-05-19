e4d7417691c1760fe4e69256066176faefa1637b
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-05-19 21:07:26 +0900

    Title: 비교 연산, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250519/비교 연산/comparison-operator.py
+++ 250519/비교 연산/comparison-operator.py
@@ -1,26 +1,3 @@
 a, b = map(int, input().split())
 
-if a>=b:
-    print(1)
-
-
-if a>b:
-    print(1)
-
-
-if a<=b:
-    print(1)
-
-
-if a<b:
-    print(1)
-
-
-if a==b:
-    print(1)
-
-
-if a!=b:
-    print(1)
-
-
+print(a>=b)
