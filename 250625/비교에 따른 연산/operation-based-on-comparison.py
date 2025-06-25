8ba294192f15e1d63a7070cbb9d74c6a59a97eb8
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-06-25 19:15:18 +0900

    Title: 비교에 따른 연산, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250625/비교에 따른 연산/operation-based-on-comparison.py
+++ 250625/비교에 따른 연산/operation-based-on-comparison.py
@@ -1,7 +1,9 @@
-a = int(input())
-b = int(input())
+arr = input()
+split_arr = arr.split()
+a = int(split_arr[0])
+b = int(split_arr[1])
 
-if a>b:
+if a > b:
     print(a*b)
 else:
-    print(b%a)
+    print(b/a)
