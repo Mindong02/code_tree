c33b882277fcb9a6baf7dcc9f77570690f3962ca
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-06-28 14:29:13 +0900

    Title: 중앙값 구하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250628/중앙값 구하기/find-the-median.py
+++ 250628/중앙값 구하기/find-the-median.py
@@ -2,11 +2,17 @@ a, b, c = map(int, input().split())
 
 if a>b:
     if c<a:
-        print(c)
+        if b<c:
+            print(c)
+        else:
+            print(b)
     else:
         print(a)
 else: # a<b -> b<c or c<b
     if b<c:
         print(b)
     else:
-        print(c)
+        if a<c:
+            print(c)
+        else:
+            print(a)
