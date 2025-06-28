da1ecb82e2a79ceec455c27baf3b215855c0ca1e
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-06-28 15:59:44 +0900

    Title: 더 긴 문자열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250628/더 긴 문자열/longer-string.py
+++ 250628/더 긴 문자열/longer-string.py
@@ -1,5 +1,11 @@
 data = input().split()
 text1  = str(data[0])
 text2  = str(data[1])
-print(text1)
-print(text2)
+a = len(text1)
+b = len(text2)
+if a<b:
+    print(text2, b)
+elif a==b:
+    print("same")
+else:
+    print(text1, a)
