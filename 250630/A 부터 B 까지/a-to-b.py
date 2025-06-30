1a1db98c3d4b02c381b3274758833e2ff4d09e11
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-06-30 12:24:15 +0900

    Title: A 부터 B 까지, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250630/A 부터 B 까지/a-to-b.py
@@ -0,0 +1,17 @@
+a,b = map(int,input().split())
+print(a, end=" ")
+while True:
+    if a<b:
+        if a % 2 == 1:
+            a*=2
+            if a >b:
+                break
+            print(a, end=" ")
+        elif a % 2 == 0:
+            a+=3
+            if a>b:
+                break
+            print(a, end=" ")
+    else:
+        break
+
