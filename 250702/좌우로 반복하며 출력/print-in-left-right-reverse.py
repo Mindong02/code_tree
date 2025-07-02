77fa9608d1c56a28dc7675e0198d27bfeb8873aa
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-07-02 15:58:05 +0900

    Title: 좌우로 반복하며 출력, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250702/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,14 @@
+a = int(input())
+
+for i in range(1,a+1):
+    if i % 2 !=0:
+        for j in range(1,a+1):
+            print(j,end="")
+        print()
+    else:
+        for j in range(a,0,-1):
+            print(j, end="")
+        print()
+
+            
+         
