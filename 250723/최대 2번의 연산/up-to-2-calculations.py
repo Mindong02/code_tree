0c2c71f84abacf651192c48c98ea74840e8d4faa
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-07-23 01:03:09 +0900

    Title: 최대 2번의 연산, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/최대 2번의 연산/up-to-2-calculations.py
+++ 250723/최대 2번의 연산/up-to-2-calculations.py
@@ -1,9 +1,8 @@
 a = int(input())
 
-for i in range(2):
-    if a % 2 ==0:
-        a= a//2
-    else:
-        a= (a+1)//2
+if a % 2 ==0:
+    a= a//2
+if a % 2 ==1:
+    a= (a+1)//2
 
 print(int(a))
