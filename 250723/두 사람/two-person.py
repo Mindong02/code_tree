c9ffea7d8a347973dd3c8509d98f3f86a540d51d
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-07-23 12:48:52 +0900

    Title: 두 사람, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250723/두 사람/two-person.py
@@ -0,0 +1,9 @@
+a1,a2= map(str,input().split())
+b1,b2= map(str,input().split())
+a1 = int(a1)
+b1 = int(b1)
+if a1>=19 or b1>=19:
+    if a2 =='M' or b2 == 'M':
+        print("1")
+else:
+    print("0")
