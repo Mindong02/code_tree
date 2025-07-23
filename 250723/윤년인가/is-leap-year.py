d9656a17c3c46d8744da68ae9661b71ea3c5e199
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-07-23 13:19:46 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/윤년인가/is-leap-year.py
+++ 250723/윤년인가/is-leap-year.py
@@ -1,10 +1,11 @@
 a = int(input())
 
 if a%4==0:
-    print("true")
-elif a%100==0:
-    if a%400!=0:
-        print("false")
+    if a%100==0:
+        if a%400!=0:
+            print("false")
+        else:
+            print("true")
     else:
         print("true")
 else:
