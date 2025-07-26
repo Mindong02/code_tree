496d56339fb22cdad484b89c336e8ab239b3edda
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-07-26 22:09:45 +0900

    Title: 청소당번 정하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250726/청소당번 정하기/cleaning-numbering.py
+++ 250726/청소당번 정하기/cleaning-numbering.py
@@ -4,12 +4,13 @@ cnt2=0
 cnt3=0
 cnt12=0
 
-for i in range(a):
-    if i%12==0:
-        cnt12+=1
-    elif i%3==0:
-        cnt3+=1
-    elif i%2==0:
-        cnt2+=1
+for i in range(a+1):
+    if i!=0:
+        if i%12==0:
+            cnt12+=1
+        elif i%3==0:
+            cnt3+=1
+        elif i%2==0:
+            cnt2+=1
 
 print(cnt2, cnt3, cnt12, end=" ")
