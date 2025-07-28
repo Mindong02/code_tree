c29b7c9073d3eb93ec007befe6aee979e27164a1
DONG MIN . DEAN <132594292+Mindong02@users.noreply.github.com>
2025-07-28 15:08:34 +0900

    Title: 홀수만 더하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250728/홀수만 더하기/add-only-odd-numbers.py
+++ 250728/홀수만 더하기/add-only-odd-numbers.py
@@ -1,14 +1,9 @@
 a = int(input())
-arr = []
-sum=0
+sum = 0
 
 for i in range(a):
-    b = input().split()
-    arr.append(b)
-
-for i in range(a):
-    if arr[i]%2==1 and arr[i]%3==0:
-        sum+=arr[i]
-
+    b = int(input())
+    if b % 2==1 and b%3==0:
+        sum+=b
 
 print(sum)
